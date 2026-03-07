8f3deb28a88827745adf567ff469b5c32336711b
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2026-03-07 21:30:14 +0700

    feat(game): change game character size

--- app/src/main/java/com/kotonosora/skyboundhopper/game/GameModels.kt
+++ app/src/main/java/com/kotonosora/skyboundhopper/game/GameModels.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.geometry.Size
 data class BirdState(
     val position: Offset = Offset(100f, 500f),
     val velocity: Float = 0f,
-    val size: Size = Size(50f, 50f)
+    val size: Size = Size(125f, 125f)
 )
 
 data class PipeState(
